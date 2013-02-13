8d1b71fbfff824b95c7598e2a032815829cbf765
Ilia Gourianov <igourianov@klick.com>
2013-02-12 23:11:53 -0500

    code cleanup

--- Slapper/SqlExtensions.cs
+++ Slapper/SqlExtensions.cs
@@ -1,70 +1,65 @@
-﻿using Slapper.Mapping;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Slapper.Reflection;
 
 namespace Slapper
 {
-    public static class SqlExtensions
-    {
-        public static int ExecuteNonQuery(this IDbConnection conn, string sql, object args = null)
-        {
-            using (var cmd = conn.CreateCommand(sql, args))
-                return cmd.ExecuteNonQuery();
-        }
+	public static class SqlExtensions
+	{
+		public static int ExecuteNonQuery(this IDbConnection conn, string sql, object args = null)
+		{
+			using (var cmd = conn.CreateCommand(sql, args))
+				return cmd.ExecuteNonQuery();
+		}
 
-        public static T ExecuteScalar<T>(this IDbConnection conn, string sql, object args = null)
-        {
-            using (var cmd = conn.CreateCommand(sql, args))
-            {
-                var ret = cmd.ExecuteScalar();
-                return ret is DBNull ? default(T) : (T)ret;
-            }
-        }
+		public static T ExecuteScalar<T>(this IDbConnection conn, string sql, object args = null)
+		{
+			using (var cmd = conn.CreateCommand(sql, args))
+			{
+				var ret = cmd.ExecuteScalar();
+				return ret is DBNull ? default(T) : (T)ret;
+			}
+		}
 
-        public static IDataReader ExecuteReader(this IDbConnection conn, string sql, object args = null, CommandBehavior behavior = CommandBehavior.SingleResult)
-        {
-            using (var cmd = conn.CreateCommand(sql, args))
-                return cmd.ExecuteReader(behavior);
-        }
+		public static IDataReader ExecuteReader(this IDbConnection conn, string sql, object args = null, CommandBehavior behavior = CommandBehavior.SingleResult)
+		{
+			using (var cmd = conn.CreateCommand(sql, args))
+				return cmd.ExecuteReader(behavior);
+		}
 
-        public static IDbCommand CreateCommand(this IDbConnection conn, string sql, object args = null, bool checkSql = true)
-        {
-            var cmd = conn.CreateCommand();
-            cmd.CommandText = sql;
-            cmd.Connection = conn; // just in case
+		public static IDbCommand CreateCommand(this IDbConnection conn, string sql, object args = null, bool checkSql = true)
+		{
+			var cmd = conn.CreateCommand();
+			cmd.CommandText = sql;
+			cmd.Connection = conn; // just in case
 
-            if (args != null)
-            {
-                foreach (var m in ParameterReader.Read(args))
-                {
-                    var p = m.Value as IDbDataParameter ?? cmd.CreateParameter(m.Key, m.Value);
-                    if (!checkSql || sql.Contains(p.ParameterName))
-                        cmd.Parameters.Add(p);
-                }
-            }
+			if (args != null)
+			{
+				foreach (var m in ParameterReader.Read(args))
+				{
+					var p = m.Value as IDbDataParameter ?? cmd.CreateParameter(m.Key, m.Value);
+					if (!checkSql || sql.Contains(p.ParameterName))
+						cmd.Parameters.Add(p);
+				}
+			}
 
-            return cmd;
-        }
+			return cmd;
+		}
 
-        public static IDbDataParameter CreateParameter(this IDbCommand cmd, string name, object value, DbType? type = null)
-        {
-            var p = cmd.CreateParameter();
-            p.ParameterName = name.StartsWith("@") ? name : "@" + name;
-            p.Value = value ?? DBNull.Value;
-            if (type != null)
-                p.DbType = type.Value;
-            return p;
-        }
+		public static IDbDataParameter CreateParameter(this IDbCommand cmd, string name, object value, DbType? type = null)
+		{
+			var p = cmd.CreateParameter();
+			p.ParameterName = name.StartsWith("@") ? name : "@" + name;
+			p.Value = value ?? DBNull.Value;
+			if (type != null)
+				p.DbType = type.Value;
+			return p;
+		}
 
-        public static IDbDataParameter CreateParameter(this IDbConnection conn, string name, object value, DbType? type = null)
-        {
-            using (var cmd = conn.CreateCommand())
-                return cmd.CreateParameter(name, value, type);
-        }
-    }
+		public static IDbDataParameter CreateParameter(this IDbConnection conn, string name, object value, DbType? type = null)
+		{
+			using (var cmd = conn.CreateCommand())
+				return cmd.CreateParameter(name, value, type);
+		}
+	}
 }
