ac2b371321cbb51629b80c528a6031538844e25b
Ilia Gourianov <igourianov@gmail.com>
2013-11-03 21:39:24 -0500

    cleaned up command parameters

--- Slapper/IDbCommandExtensions.cs
+++ Slapper/IDbCommandExtensions.cs
@@ -9,22 +9,27 @@ namespace Slapper
 {
 	public static class IDbCommandExtensions
 	{
-		public static int ExecuteNonQuery(this IDbCommand cmd, object args = null)
+		public static int ExecuteNonQuery(this IDbCommand cmd, object args)
 		{
 			SetParams(cmd, args);
 			return cmd.ExecuteNonQuery();
 		}
 
-		public static T ExecuteScalar<T>(this IDbCommand cmd, object args = null)
+		public static T ExecuteScalar<T>(this IDbCommand cmd)
 		{
-			SetParams(cmd, args);
 			var ret = cmd.ExecuteScalar();
 			if (ret is DBNull)
 				ret = null;
 			return (T)ret;
 		}
 
-		public static IDataReader ExecuteReader(this IDbCommand cmd, object args = null, CommandBehavior behavior = CommandBehavior.SingleResult)
+		public static T ExecuteScalar<T>(this IDbCommand cmd, object args)
+		{
+			SetParams(cmd, args);
+			return cmd.ExecuteScalar<T>();
+		}
+
+		public static IDataReader ExecuteReader(this IDbCommand cmd, object args, CommandBehavior behavior = CommandBehavior.SingleResult)
 		{
 			SetParams(cmd, args);
 			return cmd.ExecuteReader(behavior);
@@ -33,20 +38,20 @@ namespace Slapper
 		static void SetParams(IDbCommand cmd, object args)
 		{
 			cmd.Parameters.Clear();
+
 			if (args == null)
 				return;
 
 			foreach (var p in ParameterMapper.Read(args))
-				cmd.Parameters.Add(CreateParameter(cmd, p.Name, p.Value));
+				if (cmd.CommandText.Contains("@" + p.Name))
+					cmd.Parameters.Add(CreateParameter(cmd, p.Name, p.Value));
 		}
 
-		static IDbDataParameter CreateParameter(IDbCommand cmd, string name, object value, DbType? type = null)
+		static IDbDataParameter CreateParameter(IDbCommand cmd, string name, object value)
 		{
 			var p = cmd.CreateParameter();
 			p.ParameterName = name;
 			p.Value = value ?? DBNull.Value;
-			if (type != null)
-				p.DbType = type.Value;
 			return p;
 		}
 	}
